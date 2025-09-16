5a3bbd5b761a3392ae0a7a82bed9315d25eb97d8
akash-shashi <a.shashi.chaurasia@accenture.com>
2025-09-16 18:32:53 +0530

    fixed build issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
     'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
     'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
     'no-param-reassign': [2, { props: false }], // allow modifying properties of param
-     'import/no-extraneous-dependencies': ['error', {
+    'import/no-extraneous-dependencies': ['error', {
       devDependencies: ['**/*.test.js', '**/*.spec.js', 'playwright.config.js'],
     }],
     'xwalk/max-cells': ['error', {
